12289c20f2c1d327fe1d81c1509e576ff0ae7d1b
TEACHER\ICT02_22 <admin@admin.com>
2024-11-25 10:23:04 +0900

    commit

--- frontend-react/src/components/board/ReplyForm2.jsx
+++ frontend-react/src/components/board/ReplyForm2.jsx
@@ -1,73 +1,102 @@
 //https://htmlcssfreebies.com/bootstrap-5-comment-form-section-component/
 
-function ReplyForm2({ handleInputChange, replyClick }) {
-    return (
-        // HTML<!-- Comment Form 1 - HCF Bootstrap 5 Component -->
-        <div className="card my-4">
-            <h5 className="card-header">Leave a Comment:</h5>
-            <div className="card-body">
-                <form method="POST">
-                    <div className="form-group">
-                        <div className="row mb-3">
-                            <div className="col-12 col-md-6">
-                                <label htmlFor="email" className="form-label">
-                                    이름 <span className="text-danger">*</span>
-                                </label>
-                                <div className="input-group">
-                                    <span className="input-group-text"></span>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="name"
-                                        name="name"
-                                        required
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                            </div>
+function ReplyForm2({ board }) {
+  //댓글
+  const [reply, setReply] = useState(...board);
 
-                            <div className="col-12 col-md-6">
-                                <label htmlFor="email" className="form-label">
-                                    제목 <span className="text-danger">*</span>
-                                </label>
-                                <div className="input-group">
-                                    <span className="input-group-text"></span>
-                                    <input
-                                        type="title"
-                                        className="form-control"
-                                        id="title"
-                                        name="title"
-                                        required
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-12">
-                            <label for="comment" className="form-label">
-                                Comment <span className="text-danger">*</span>
-                            </label>
-                            <textarea
-                                className="form-control"
-                                id="content"
-                                rows="3"
-                                required=""
-                                name="content"
-                                onChange={handleInputChange}
-                            ></textarea>
-                        </div>
-                        <button
-                            onClick={replyClick}
-                            type="submit"
-                            className="btn btn-primary mt-3"
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </form>
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setReply({ ...board, [name]: value });
+  };
+
+  useEffect(() => {
+    console.log(reply);
+  }, []);
+
+  const replyClick = (e) => {
+    e.preventDefault(); // 기존에 링크 동작을 하지 말아라
+    console.log(board);
+
+    boardService
+      .replyShape(board)
+      .then((response) => {
+        setSubmitted(true);
+        console.log(response.data);
+
+        navigate(`/board`);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    // HTML<!-- Comment Form 1 - HCF Bootstrap 5 Component -->
+    <div className="card my-4">
+      <h5 className="card-header">Leave a Comment:</h5>
+      <div className="card-body">
+        <form method="POST">
+          <div className="form-group">
+            <div className="row mb-3">
+              <div className="col-12 col-md-6">
+                <label htmlFor="email" className="form-label">
+                  이름 <span className="text-danger">*</span>
+                </label>
+                <div className="input-group">
+                  <span className="input-group-text"></span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    required
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+
+              <div className="col-12 col-md-6">
+                <label htmlFor="email" className="form-label">
+                  제목 <span className="text-danger">*</span>
+                </label>
+                <div className="input-group">
+                  <span className="input-group-text"></span>
+                  <input
+                    type="title"
+                    className="form-control"
+                    id="title"
+                    name="title"
+                    required
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
             </div>
-        </div>
-    );
+            <div className="col-12">
+              <label for="comment" className="form-label">
+                Comment <span className="text-danger">*</span>
+              </label>
+              <textarea
+                className="form-control"
+                id="content"
+                rows="3"
+                required=""
+                name="content"
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+            <button
+              onClick={replyClick}
+              type="submit"
+              className="btn btn-primary mt-3"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ReplyForm2;
--- frontend-react/src/pages/BoardUpdatePage.jsx
+++ frontend-react/src/pages/BoardUpdatePage.jsx
@@ -1,162 +1,129 @@
-import { useState, useEffect } from 'react';
-import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
-import boardService from '../services/board/BoardService';
-import ReplyForm from '../components/board/ReplyForm';
-import ReplyForm2 from '../components/board/ReplyForm2';
+import { useState, useEffect } from "react";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import boardService from "../services/board/BoardService";
+import ReplyForm from "../components/board/ReplyForm";
+import ReplyForm2 from "../components/board/ReplyForm2";
 
 function BoardUpdatePage() {
-    const initBoardState = {
-        name: '',
-        title: '',
-        content: '',
+  const initBoardState = {
+    name: "",
+    title: "",
+    content: "",
+  };
+  const { id } = useParams();
+  
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setBoard({ ...board, [name]: value });
+  };
+  const [board, setBoard] = useState(initBoardState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const cancelClick = () => {
+    navigate(`/board`);
+  };
+  //useEffect 처음 한번만 랜더링
+  useEffect(() => {
+    boardService
+      .get(id)
+      .then((response) => {
+        console.log(response.data);
+        setBoard(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const updateClick = (e) => {
+    let data = {
+      name: board.name,
+      title: board.title,
+      content: board.content,
     };
-    const { id } = useParams();
 
-    const [board, setBoard] = useState(initBoardState);
-    const [submitted, setSubmitted] = useState(false);
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setBoard({ ...board, [name]: value });
-    };
-
-    const cancelClick = () => {
+    boardService
+      .update(id, data)
+      .then((response) => {
+        setSubmitted(true);
+        console.log(response.data);
+        //페이지 이동
         navigate(`/board`);
-    };
-    //useEffect 처음 한번만 랜더링
-    useEffect(() => {
-        boardService
-            .get(id)
-            .then((response) => {
-                console.log(response.data);
-                setBoard(response.data);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    }, []);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    const navigate = useNavigate();
-
-    const updateClick = (e) => {
-        let data = {
-            name: board.name,
-            title: board.title,
-            content: board.content,
-        };
-
-        boardService
-            .update(id, data)
-            .then((response) => {
-                setSubmitted(true);
-                console.log(response.data);
-                //페이지 이동
-                navigate(`/board`);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    };
-
-    //댓글
-    const [reply, setReply] = useState({
-        name: '',
-        content: '',
-    });
-
-    const replyClick = (e) => {
-        e.preventDefault(); // 기존에 링크 동작을 하지 말아라
-        console.log(board);
-
-        boardService
-            .replyShape(board)
-            .then((response) => {
-                setSubmitted(true);
-                console.log(response.data);
-
-                navigate(`/board`);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    };
-
-    return (
-        <div>
-            <div className="container mt-3">
-                <div className="container">
-                    <div className="row">
-                        <div className="card col-md-6 offset-md-3 offset-md-3">
-                            <h3 className="text-center mt-3">내용상세</h3>
-                            <div className="card-body">
-                                <div className="form-group">
-                                    <label> Type </label>
-                                    <select
-                                        placeholder="type"
-                                        className="form-control"
-                                    >
-                                        <option value="1">자유게시판</option>
-                                        {/* <option value="2">질문과 답변</option> */}
-                                    </select>
-                                </div>
-                                <div className="form-group mt-3">
-                                    <label> Name </label>
-                                    <input
-                                        type="text"
-                                        placeholder="이름을 넣으시오"
-                                        name="name"
-                                        className="form-control"
-                                        value={board.name}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="form-group mt-3">
-                                    <label> Title </label>
-                                    <input
-                                        placeholder="제목을 넣으시오."
-                                        name="title"
-                                        className="form-control"
-                                        value={board.title}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="form-group mt-3 mb-3">
-                                    <label> Content </label>
-
-                                    <textarea
-                                        placeholder="내용을 적으시오"
-                                        name="content"
-                                        className="form-control"
-                                        value={board.content}
-                                        onChange={handleInputChange}
-                                        rows="10"
-                                    />
-                                </div>
-                                <button
-                                    className="btn btn-success"
-                                    onClick={updateClick}
-                                >
-                                    Update
-                                </button>
-                                <button
-                                    className="btn btn-danger"
-                                    style={{ marginLeft: '10px' }}
-                                    onClick={cancelClick}
-                                >
-                                    Cancel
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <div className="container mt-3">
+        <div className="container">
+          <div className="row">
+            <div className="card col-md-6 offset-md-3 offset-md-3">
+              <h3 className="text-center mt-3">내용상세</h3>
+              <div className="card-body">
+                <div className="form-group">
+                  <label> Type </label>
+                  <select placeholder="type" className="form-control">
+                    <option value="1">자유게시판</option>
+                    {/* <option value="2">질문과 답변</option> */}
+                  </select>
                 </div>
-                <hr />
-                <ReplyForm2
-                    handleInputChange={handleInputChange}
-                    replyClick={replyClick}
-                ></ReplyForm2>
+                <div className="form-group mt-3">
+                  <label> Name </label>
+                  <input
+                    type="text"
+                    placeholder="이름을 넣으시오"
+                    name="name"
+                    className="form-control"
+                    value={board.name}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-group mt-3">
+                  <label> Title </label>
+                  <input
+                    placeholder="제목을 넣으시오."
+                    name="title"
+                    className="form-control"
+                    value={board.title}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-group mt-3 mb-3">
+                  <label> Content </label>
+
+                  <textarea
+                    placeholder="내용을 적으시오"
+                    name="content"
+                    className="form-control"
+                    value={board.content}
+                    onChange={handleInputChange}
+                    rows="10"
+                  />
+                </div>
+                <button className="btn btn-success" onClick={updateClick}>
+                  Update
+                </button>
+                <button
+                  className="btn btn-danger"
+                  style={{ marginLeft: "10px" }}
+                  onClick={cancelClick}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-    );
+        <hr />
+        <ReplyForm2 board={board}></ReplyForm2>
+      </div>
+    </div>
+  );
 }
 
 export default BoardUpdatePage;
